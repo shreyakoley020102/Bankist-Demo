ccfc64c3d1198ffa6e0561474107ff1d8c1d1880
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-01-10 18:46:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -96,3 +96,36 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+const eurToUsd = 1.1;
+
+const movemeentsUSD = movements.map(mov => mov * eurToUsd);
+
+/* 
+const movemeentsUSD = movements.map(function (mov) {
+  return mov * eurToUsd;
+});
+ */
+
+console.log(movements);
+console.log(movemeentsUSD);
+
+const movemeentsUSDfor = [];
+for (const mov of movements) movemeentsUSDfor.push(mov * eurToUsd);
+console.log(movemeentsUSDfor);
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'}${Math.abs(
+      mov
+    )}`
+  /*
+  if (mov > 0) {
+    return `Movement ${i + 1}: You deposited ${mov}`;
+  } else {
+    return `Movement ${i + 1}: You withdrew ${Math.abs(mov)}`;
+  }
+  */
+);
+
+console.log(movementsDescriptions);
