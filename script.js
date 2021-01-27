f69d5105b4481737606c5e64891208d90cdedfdf
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-01-27 21:01:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -182,6 +182,15 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+  }
+});
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
 
