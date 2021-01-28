e4d615b06b82e5c6d9a99ed2fd5e24473b744867
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-01-28 10:51:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -227,6 +227,7 @@ let sorted = false;
 btnSort.addEventListener('click', function (e) {
   e.preventDefault();
   displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 });
 
 /////////////////////////////////////////////////
