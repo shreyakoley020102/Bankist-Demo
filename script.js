6d9eb6349893c0c2e7438c403ec693d438823ebc
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-01-28 09:17:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -347,8 +347,15 @@ console.log(accountMovements);
 const allMovements = accountMovements.flat();
 console.log(allMovements);
 
+// flat
 const overalBalance = accounts
   .map(acc => acc.movements)
   .flat()
   .reduce((acc, mov) => acc + mov, 0);
 console.log(overalBalance);
+
+// flatmap
+const overalBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance2);
