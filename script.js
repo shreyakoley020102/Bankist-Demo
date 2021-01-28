bd87658b17f7d3bc06317345b03ee318b20d0719
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-01-28 10:50:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -222,9 +222,11 @@ btnClose.addEventListener('click', function (e) {
   inputCloseUsername.value = inputClosePin.value = '';
 });
 
+let sorted = false;
+
 btnSort.addEventListener('click', function (e) {
   e.preventDefault();
-  displayMovements(currentAccount.movements, true);
+  displayMovements(currentAccount.movements, !sorted);
 });
 
 /////////////////////////////////////////////////
